a1531f216c5b62b5d2daee3577d27a6f02fd447a
dave-wind <18119651372@163.com>
2020-08-19 21:20:01 +0800

    init storybook

---
+++ .storybook/main.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-viewport",
+    '@storybook/addon-storysource',
+    "@storybook/addon-knobs",
+		{ name: "@storybook/addon-docs", options: { configureJSX: true } },
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+    "@storybook/preset-create-react-app"
+  ],
+  webpackFinal: async (config) => {
+		config.module.rules.push({
+			test: /\.(ts|tsx)$/,
+			use: [
+				{
+					loader: require.resolve("react-docgen-typescript-loader"),
+				},
+			],
+		});
+		config.resolve.extensions.push(".ts", ".tsx");
+		return config;
+	},
+}
