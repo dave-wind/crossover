62cc6fe9d7be26013056be501f0632f1bb974139
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-15 17:21:07 +0000

    Deploy dave-wind/crossover to github.com/dave-wind/crossover.git:gh-pages

---
+++ precache-manifest.b702ebec9f6ad8c0110ce1244064c143.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bac6124edf29ae0bce1588574e74c8e",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e045dda810ff937c5064.bundle.js"
+  },
+  {
+    "url": "runtime~main.e045dda810ff937c5064.bundle.js"
+  },
+  {
+    "revision": "1421d6873df9f5b6601d",
+    "url": "static/css/main.17b2fa90.chunk.css"
+  },
+  {
+    "revision": "c9df018b5da8b42e44fb055c822ee0f0",
+    "url": "static/media/code-brackets.c9df018b.svg"
+  },
+  {
+    "revision": "771456e0755932aa3ba4e1d75cd8ebc8",
+    "url": "static/media/colors.771456e0.svg"
+  },
+  {
+    "revision": "84015590ebc5913b047ca167030bd675",
+    "url": "static/media/comments.84015590.svg"
+  },
+  {
+    "revision": "ea9e1b4e37fe3704d1babe76bd93694e",
+    "url": "static/media/direction.ea9e1b4e.svg"
+  },
+  {
+    "revision": "0a3e69dee1c2b27c579a8c193a9674e1",
+    "url": "static/media/flow.0a3e69de.svg"
+  },
+  {
+    "revision": "4e48689ac2fb8cc3861a092a1da3f3cd",
+    "url": "static/media/plugin.4e48689a.svg"
+  },
+  {
+    "revision": "0d87ede8c57dbd52b40b3d647dbfbc61",
+    "url": "static/media/repo.0d87ede8.svg"
+  },
+  {
+    "revision": "f725b73e6d5c667f26a3d010f72cce84",
+    "url": "static/media/stackalt.f725b73e.svg"
+  },
+  {
+    "url": "vendors~main.e045dda810ff937c5064.bundle.js"
+  },
+  {
+    "url": "vendors~main.e045dda810ff937c5064.bundle.js.LICENSE.txt"
+  }
+]);
